b575b50f2f8b756fdb60795d4b193fcbd0a432d6
Igor <igopood33@gmail.com>
2024-04-27 23:46:07 +0200

    Added repr, eq, gt, lt functions

--- objectOrientedProgramming/SSHLogEntry.py
+++ objectOrientedProgramming/SSHLogEntry.py
@@ -4,6 +4,10 @@ import re
 import abc
 
 
+class ValueNotFound(Exception):
+    pass
+
+
 class SSHLogEntry(metaclass=abc.ABCMeta):
     def __init__(self, log):
         pattern = r'^(\w{3}\s+\d{1,2}\s\d{2}:\d{2}:\d{2}) (LabSZ) sshd\[(\d+)\]: (.*)$'
@@ -15,7 +19,6 @@ class SSHLogEntry(metaclass=abc.ABCMeta):
                 date.replace(year=2024)
             else:
                 date = date.replace(year=2023)
-            timestamp = date
             hostname = match.group(2)
             sshd = match.group(3)
             msg = match.group(4)
@@ -24,17 +27,55 @@ class SSHLogEntry(metaclass=abc.ABCMeta):
             self.msg = msg
             self.pid_number = sshd
             self.hostname = hostname
+            self._raw_log = log
+        else:
+            print("nie udalo sie zmatchowac loga")
 
     def __str__(self):
         return (f'Data: {self.date} | Numer PID: {self.pid_number} | Wiadomość: {self.msg} '
                 f'| Nazwa hosta: {self.hostname}')
+
+    def __repr__(self):
+        return (f"{type(self).__name__}(date={self.date}, msg='{self.msg}', "
+                f"pid_number={self.pid_number}, hostname='{self.hostname}', "
+                f"has_ip='{self.has_ip}', )")
+
+    def __eq__(self, other):
+        if isinstance(other, SSHLogEntry):
+            return self._raw_log == other.get_raw_log()
+        return NotImplemented
+
+    def __lt__(self, other):
+        if isinstance(other, SSHLogEntry):
+            return self.date < other.date
+        return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, SSHLogEntry):
+            return self.date > other.date
+        return NotImplemented
+
+    @property
+    def has_ip(self):
+        try:
+            self.get_ipv4s()
+        except ValueNotFound:
+            return False
+        return True
 
     @abc.abstractmethod
     def validate(self):
         pass
 
     def get_ipv4s(self):
-        return ipaddress.IPv4Address(str(self.get_ipv4s_from_log(self.msg).group(1)))
+        ipv4 = self.get_ipv4s_from_log(self.msg)
+        if ipv4 is not None:
+            return ipaddress.IPv4Address(str(ipv4.group(1)))
+        else:
+            raise ValueNotFound
+
+    def get_raw_log(self):
+        return self._raw_log
 
     @classmethod
     def get_ipv4s_from_log(cls, log):
@@ -44,19 +85,18 @@ class SSHLogEntry(metaclass=abc.ABCMeta):
         return match
 
     @classmethod
-    def get_message_type(cls, log):
-        if re.search("Failed password", log):
+    def get_message_type(cls, msg):
+        if re.search("Failed password", msg):
             return "failed login"
-        elif re.search("Accepted password", log):
+        elif re.search("Accepted password", msg):
             return "successful login"
-        elif re.search("error", log):
+        elif re.search("error", msg):
             return "error"
         else:
             return "different"
 
 
 class IncorrectPasswordEntry(SSHLogEntry):
-
     def __init__(self, log):
         super().__init__(log)
 
@@ -65,7 +105,6 @@ class IncorrectPasswordEntry(SSHLogEntry):
 
 
 class CorrectPasswordEntry(SSHLogEntry):
-
     def __init__(self, log):
         super().__init__(log)
 
@@ -74,7 +113,6 @@ class CorrectPasswordEntry(SSHLogEntry):
 
 
 class ErrorEntry(SSHLogEntry):
-
     def __init__(self, log):
         super().__init__(log)
 
@@ -83,7 +121,6 @@ class ErrorEntry(SSHLogEntry):
 
 
 class OtherEntry(SSHLogEntry):
-
     def __init__(self, log):
         super().__init__(log)
 
